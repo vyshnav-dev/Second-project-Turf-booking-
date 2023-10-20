945aaae5297e375cf896b9ac0d59ce0a12533a4c
Vyshnav C <devvyshnav90@gmail.com>
2023-10-19 18:06:01 -1200

    server change

--- backend/server.js
+++ backend/server.js
@@ -72,11 +72,11 @@ app.use("/api/owner", ownerRoutes);
 
 if(process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  const newPath=path.join(__dirname,"..")
-  app.use(express.static(path.join(newPath, 'frontend/dist')));
+  
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
  
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(newPath, 'frontend', 'dist', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 } else{
   app.get("/", (req, res) => res.send("Server is ready"));
 }
